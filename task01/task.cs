84d7e8dfa9ea627d1e5f97e639eb55ae3d182e30
Kirill K <francesc12008@gmail.com>
2022-05-11 14:59:52 +0300

    initial commit

--- task01/task.cs
+++ task01/task.cs
@@ -1,1 +1,2 @@
-﻿
+﻿string[] arrayFirst = new string[10] {"Renault", "KIA", "Ford", "Skoda", "Lada", "Nissan", "JAC", "BMW", "UAZ", "Chevrolet"};
+string[] arraySecond = new string[arrayFirst.Length];
