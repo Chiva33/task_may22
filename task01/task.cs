0aec9b5b5c0fd75739d102c7c91c6ee7df39e9f0
Kirill K <francesc12008@gmail.com>
2022-05-11 15:04:52 +0300

    added arraySecondOut

--- task01/task.cs
+++ task01/task.cs
@@ -1,2 +1,14 @@
 ﻿string[] arrayFirst = new string[10] {"Renault", "KIA", "Ford", "Skoda", "Lada", "Nissan", "JAC", "BMW", "UAZ", "Chevrolet"};
 string[] arraySecond = new string[arrayFirst.Length];
+void arraySecondOut(string[] arrayFirst, string[] arraySecond)
+{
+    int count = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+    if(arrayFirst[i].Length <= 3)
+        {
+        arraySecond[count] = arrayFirst[i];
+        count++;
+        }
+    }
+}
